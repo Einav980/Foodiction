1497632879139b36e8846bc8dadc5c42462590b0
kokolight <tsegal97@gmail.com>
2021-11-28 23:15:57 +0200

    added docs and added material design

--- app/src/main/java/com/example/foodiction/MainActivity.java
+++ app/src/main/java/com/example/foodiction/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.foodiction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolBar);
     }
 
-    public void SearchButton(View view) {
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
